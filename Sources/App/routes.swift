62ba5827091c3abe37376c97b0e1c96fe2003c67
saiashish9 <saiashish3760@gmail.com>
2022-02-25 18:08:19 +0530

    completed

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,11 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("test", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+          }
+        return "Hello, \(name)"
+    }
 }
